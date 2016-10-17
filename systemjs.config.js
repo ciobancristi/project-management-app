9768228573d103cf0d7713f49a157b0832161edc
Cristi Cioban <ciobancristi@gmail.com>
2016-10-17 21:48:13 +0300

    update to angular 2.1

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,60 +1,50 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
-(function(global) {
-  // map tells the System loader where to look for things
-  var map = {
-    'app':                        'app', // 'dist',
-    '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs',
-    'pouchdb':                    'node_modules/pouchdb/dist',
-    'raven-js':                   'node_modules/raven-js/dist',
-    'angular2-toaster':           'node_modules/angular2-toaster',
-    'angular2-highcharts':        'node_modules/angular2-highcharts/dist',
-    'highcharts/highstock.src':   'node_modules/highcharts/highstock.js',
-    'ng2-bootstrap':              'node_modules/ng2-bootstrap/',
-    'moment':                     'node_modules/moment/moment.js'
-  };
-  // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-    'pouchdb':                    { main: 'pouchdb.js', defaultExtension: 'js' },
-    'raven-js':                   { main: 'raven.js', defaultExtension: 'js' },
-    'angular2-toaster':           { main: 'angular2-toaster.js', defaultExtension: 'js' },
-    'angular2-highcharts':        { main: './index.js',defaultExtension: 'js'},
-    'ng2-bootstrap':              { main: './ng2-bootstrap', defaultExtension: 'js'}
-  };
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
-  ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  var config = {
-    map: map,
-    packages: packages
-  };
-  System.config(config);
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs': 'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'pouchdb': 'npm:pouchdb/dist',
+      'raven-js': 'npm:raven-js/dist',
+      'angular2-toaster': 'npm:angular2-toaster',
+      'angular2-highcharts': 'npm:angular2-highcharts/dist',
+      'highcharts/highstock.src': 'npm:highcharts/highstock.js',
+      'ng2-bootstrap': 'npm:ng2-bootstrap/',
+      'moment': 'npm:moment/moment.js'
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      },
+      'pouchdb': { main: 'pouchdb.js', defaultExtension: 'js' },
+      'raven-js': { main: 'raven.js', defaultExtension: 'js' },
+      'angular2-toaster': { main: 'angular2-toaster.js', defaultExtension: 'js' },
+      'angular2-highcharts': { main: './index.js', defaultExtension: 'js' },
+      'ng2-bootstrap': { main: './ng2-bootstrap', defaultExtension: 'js' }
+    }
+  });
 })(this);
